3596039e8d5be14ddfadb03dea79d0eb2a7ad029
Evgeny Ladyzhenskiy <7384093@gmail.com>
2016-05-26 19:33:11 +0300

    number check

--- build/js/check.js
+++ build/js/check.js
@@ -2,4 +2,11 @@ function getMessage(a,b) {
   if (typeof a === "boolean") {
     console.log('Я попал в ' + b);
   }
+  else if (typeof a === "number") {
+    console.log('Я прыгнул на ' + a*100 + ' сантиметров');
+  }
+  else {
+    console.log('Я никуда не попал');
+  }
+
 };
