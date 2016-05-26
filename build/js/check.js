d8e5be45f1e204bc23d48cb4264f8ed932375074
Evgeny Ladyzhenskiy <7384093@gmail.com>
2016-05-26 18:10:42 +0300

    check.js added

--- build/js/check.js
+++ build/js/check.js
@@ -1,12 +1,2 @@
-<<<<<<< HEAD
-function getMessage() {};
-=======
-function getMessage(a:*, b:*=):string {
-  if (a = boolean) {
-    return('Я попал в ' + b);
-  }
-  else {
-    return('Я никуда не попал');
-  }
+function getMessage(a,b) {
 };
->>>>>>> 1bf84cdc571248a0a5d82db0381c04a9e787c85b
