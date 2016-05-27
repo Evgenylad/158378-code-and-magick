be9b8e9eccf17c96f63e334fe90fb1a40257cd95
Evgeny Ladyzhenskiy <7384093@gmail.com>
2016-05-27 18:38:58 +0300

    check.js totally revised

--- build/js/check.js
+++ build/js/check.js
@@ -1,31 +1,34 @@
 function getMessage(a,b) {
 
-    if (Array.isArray(a)) {
-      var total = a.reduce(function(sum, current) {
-        return sum + current;
-      });
-      console.log('Я прошел ' + total + ' шагов');
+  if (!Array.isArray(a)) {
+    switch (typeof a) {
+      case "boolean":
+        console.log('Я попал в ' + b);
+        break;
+
+      case "number":
+        console.log('Я прыгнул на ' + a*100 + ' сантиметров');
+        break;
+
+      default:
+        console.log('Я никуда не попал');
+      }
     }
 
-    else if (Array.isArray(a) && Array.isArray(b)) {
-      var ab = a.map(function (num, i) {
-        return num*b[i];
-      });
-      console.log('Я прошел ' + lenght + ' метров');
-    }
+  else if (Array.isArray(a) && !Array.isArray(b)) {
+    var total = a.reduce(function(sum, current) {
+      return sum + current;
+     });
+    console.log('Я прошел ' + total + ' шагов');
+  }
 
-    else {
-      switch (typeof a) {
-        case "boolean":
-          console.log('Я попал в ' + b);
-          break;
-
-        case "number":
-          console.log('Я прыгнул на ' + a*100 + ' сантиметров');
-          break;
-
-        default:
-          console.log('Я никуда не попал');
-    }
+  else {
+    var ab = a.map(function (num, i) {
+      return num*b[i];
+    });
+    var lenght = ab.reduce(function(sum, current) {
+      return sum + current;
+    });
+    console.log('Я прошел ' + lenght + ' метров');
   }
 }
