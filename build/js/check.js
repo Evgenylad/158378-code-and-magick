ebc32c3c7e813a5b745c596a70391a1019ee5a44
Evgeny Ladyzhenskiy <7384093@gmail.com>
2016-05-27 07:50:04 +0300

    array check

--- build/js/check.js
+++ build/js/check.js
@@ -2,9 +2,16 @@ function getMessage(a,b) {
   if (typeof a === "boolean") {
     console.log('Я попал в ' + b);
   }
+
+  else if (typeof a.isArray) {
+    for (var i = 0, sum = 0; i < a.length; sum += a[i++]);
+    console.log('Я прошел ' + sum + ' шагов');
+  }
+
   else if (typeof a === "number") {
     console.log('Я прыгнул на ' + a*100 + ' сантиметров');
   }
+
   else {
     console.log('Я никуда не попал');
   }
