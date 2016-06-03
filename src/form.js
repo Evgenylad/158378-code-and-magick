d1f117181d4ad362eef918ace85ab89d3c042614
Evgeny Ladyzhenskiy <7384093@gmail.com>
2016-06-03 14:27:07 +0300

    issue with refresh in browser

--- src/form.js
+++ src/form.js
@@ -14,4 +14,18 @@
     evt.preventDefault();
     formContainer.classList.add('invisible');
   };
+
+  var form = document.forms[1];
+  var range = form.elements['review-mark'];
+  var text = document.getElementById('review-text');
+  var checkingRange = function() {
+    if(range.value < 3) {
+      text.removeAttribute('disabled');
+      console.log(text);
+
+    }
+  };
+
+  checkingRange();
+
 })();
