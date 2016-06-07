4145443d302ed56207403b1a45f8841f63af5e73
Evgeny Ladyzhenskiy <7384093@gmail.com>
2016-06-07 15:19:05 +0300

    input length validation added + validation.onfocus out added

--- src/form.js
+++ src/form.js
@@ -8,90 +8,124 @@
   formOpenButton.onclick = function(evt) {
     evt.preventDefault();
     formContainer.classList.remove('invisible');
-    textInput.addEventListener('input', textInputOninput);
-    nameInput.addEventListener('input', nameInputOninput);
-  //  addEventListener('input', inputFieldsOninput);
+    _addEventListeners();
   };
 
   formCloseButton.onclick = function(evt) {
     evt.preventDefault();
     formContainer.classList.add('invisible');
-    textInput.removeEventListener('input', textInputOninput);
-    nameInput.removeEventListener('input', nameInputOninput);
+    _removeEventListeners();
   //  addEventListener('input', inputFieldsOninput);
   };
+
   var form = document.querySelector('.review-form');
   var ratingRadioButtons = document.getElementsByName('review-mark');
+
   var textInput = document.getElementById('review-text');
   var nameInput = document.getElementById('review-name');
-  var submitButton = form.querySelector('.review-submit');
-  var reviewFieldsContainer = form.querySelector('.review-fields');
-  var reviewMarkInput = form.elements['review-mark'];
-  var nameLabel = form.querySelector('.review-fields-name');
+
   var textLabel = form.querySelector('.review-fields-text');
+  var nameLabel = form.querySelector('.review-fields-name');
+  var defaultLabelDisplay = getComputedStyle(nameLabel).display;
+
+
+  var reviewFieldsContainer = form.querySelector('.review-fields');
+
+  var submitButton = form.querySelector('.review-submit');
+  var reviewMarkInput = form.elements['review-mark'];
+
 
   nameInput.setAttribute('required', true);
-// name.setCustomValidity('Please tell us your real name');
-//textInput.setCustomValidity('We\'d like to know why did you give us so low rating. Please fill the form.');
-  _disableElement(submitButton);
-  for(var i = 0; i < ratingRadioButtons.length; i++) {
-    ratingRadioButtons[i].onchange = function() {
-      var currentValue = parseInt(reviewMarkInput.value, 10);
-      textInput.removeAttribute('disabled');
-      if(currentValue < 3) {
-        textInput.setAttribute('required', true);
-      } else {
-        textInput.removeAttribute('required');
-      }
-    };
+  _disableForm();
+
+  function onRadioChange() {
+    var currentValue = parseInt(reviewMarkInput.value, 10);
+
+    if(currentValue < 3) {
+      textInput.setAttribute('required', true);
+    } else {
+      textInput.removeAttribute('required');
+    }
+
+    validateForm();
   }
 
-  var nameInputOninput = function() {
-    var nameInputLength = nameInput.value.length;
-    if (nameInputLength > 0) {
-      _validation(nameInput, nameLabel);
-    } else if (nameInputLength > 0 && textInputLength > 0) {
-      hideReviewField();
-      _enableElement(submitButton);
-    } else {
-      _disableElement(submitButton);
-      nameLabel.style.visibility = 'visible';
-      nameInput.setCustomValidity('Please tell us your real name');
-      reviewFieldsContainer.style.visibility = 'visible';
+  function _addEventListeners() {
+    nameInput.addEventListener('focusout', validateNameInput);
+    for(var i = 0; i < ratingRadioButtons.length; i++) {
+      ratingRadioButtons[i].onchange = onRadioChange;
     }
-  };
+    textInput.addEventListener('focusout', validateTextInput);
+    nameInput.addEventListener('focusout', validateNameInput);
+  }
 
-  var textInputOninput = function() {
-    var textInputLength = textInput.value.length;
-    if (textInputLength > 0 && textInput.required) {
-      _validation(textInput, textLabel);
-      hideReviewField();
-    } else if (textInputLength > 0) {
-      _validation(textInput, textLabel);
-      _enableElement(submitButton);
-    } else {
-      textInput.setCustomValidity('We\'d like to know why did you give us so low rating. Please fill the form.');
-      _disableElement(submitButton);
-      textLabel.style.visibility = 'visible';
-      reviewFieldsContainer.style.visibility = 'visible';
+  function _removeEventListeners() {
+
+    for(var i = 0; i < ratingRadioButtons.length; i++) {
+      ratingRadioButtons[i].onchange = null;
     }
-  };
+    textInput.removeEventListener('focusout', validateTextInput);
+    nameInput.removeEventListener('focusout', validateNameInput);
+  }
 
-  var hideReviewField = function() {
+  function validateNameInput() {
+
+    if (_checkField(nameInput)) {
+      nameLabel.style.display = 'none';
+
+      nameInput.setCustomValidity('');
+    } else {
+      nameLabel.style.display = defaultLabelDisplay;
+
+      nameInput.setCustomValidity('We\'d like to know your name');
+    }
+
+    validateForm();
+  }
+
+
+  function validateTextInput() {
+
+    if (_checkField(textInput)) {
+      textLabel.style.display = 'none';
+    } else {
+      textLabel.style.display = defaultLabelDisplay;
+
+      textInput.setCustomValidity('We\'d like to know why did you rate us so low. Please let us know what you\'d like to change at the game');
+    }
+
+    validateForm();
+  }
+
+
+  function validateForm() {
+    if (_checkField(nameInput) && _checkField(textInput)) {
+      _enableForm();
+    } else {
+      _disableForm();
+    }
+  }
+
+
+  // Disabling form
+  function _disableForm() {
+    reviewFieldsContainer.style.visibility = 'visible';
+    submitButton.setAttribute('disabled', 'disabled');
+  }
+
+
+  // Enabling form
+  function _enableForm() {
     reviewFieldsContainer.style.visibility = 'hidden';
-  };
-
-
-// Disabling element
-  function _disableElement(disabledElement) {
-    disabledElement.setAttribute('disabled', 'disabled');
+    submitButton.removeAttribute('disabled');
   }
-// Enabling element
-  function _enableElement(enableElement) {
-    enableElement.removeAttribute('disabled');
-  }
-// Validating and hiding .review-fields's children label if true.
-  function _validation(input, label) {
-    label.style.visibility = 'hidden';
+
+  function _checkField(input) {
+    var inputLength = input.value.length;
+    if (inputLength < 1 && input.required) {
+      return false;
+    } else {
+      return true;
+    }
   }
 })();
