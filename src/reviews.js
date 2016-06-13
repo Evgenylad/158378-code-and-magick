cf981541d6dca72eb9294ab3eb06677b3c22ca3e
Evgeny Ladyzhenskiy <7384093@gmail.com>
2016-06-13 11:17:16 +0300

    .reviews-filter hiden

---
+++ src/reviews.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var reviewsFilter = document.querySelector('.reviews-filter');
+console.log(reviewsFilter);
+reviewsFiltersHide();
+
+function reviewsFiltersHide() {
+  reviewsFilter.classList.add('invisible');
+  console.log(reviewsFilter);
+}
