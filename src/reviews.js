3106b0ac73608ad649591f76b0d3927fe0869150
Evgeny Ladyzhenskiy <7384093@gmail.com>
2016-06-13 14:17:16 +0300

    .reviews added to the page

--- src/reviews.js
+++ src/reviews.js
@@ -1,10 +1,35 @@
 'use strict';
 
 var reviewsFilter = document.querySelector('.reviews-filter');
-console.log(reviewsFilter);
+var reviewsContainer = document.querySelector('.reviews-list');
+var templateElement = document.querySelector('template');
+var elementToClone;
+
 reviewsFiltersHide();
 
 function reviewsFiltersHide() {
   reviewsFilter.classList.add('invisible');
   console.log(reviewsFilter);
 }
+
+if ('content' in templateElement) {
+  elementToClone = templateElement.content.querySelector('.review');
+} else {
+  elementToClone = templateElement.querySelector('.review');
+}
+
+/**
+*@param {Object} data
+*@param {HTMLElement} container
+*@return {HTMLElement}
+*/
+
+var getHotelElement = function(data, container) {
+  var element = elementToClone.cloneNode(true);
+  element.querySelector('.review-text').textContent = data.description;
+  container.appendChild(element);
+  return element;
+};
+window.reviews.forEach(function(review) {
+  getHotelElement(review, reviewsContainer);
+});
