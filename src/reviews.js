b16e8809cd8ad1d357e4a1abdb7a28567ef2dbcd
Evgeny Ladyzhenskiy <7384093@gmail.com>
2016-06-14 23:54:37 +0300

    changed method of adding element

--- src/reviews.js
+++ src/reviews.js
@@ -22,12 +22,13 @@ if ('content' in templateElement) {
 var getReviewElement = function(data, container) {
   var element = elementToClone.cloneNode(true);
   var picture = element.querySelector('.review-author');
+  var avatarImage = new Image();
   element.querySelector('.review-text').textContent = data.description;
   container.appendChild(element);
 
   function loadImage(url, onSuccess, onFailure) {
     // грузим картинку
-    avatarImage.src = data.author.picture;
+    avatarImage.src = url;
     // если всё хорошо
     avatarImage.onload = function() {
       onSuccess();
@@ -39,8 +40,6 @@ var getReviewElement = function(data, container) {
     };
   }
 
-  var avatarImage = new Image();
-
   loadImage(data.author.picture, function() {
     picture.src = data.author.picture;
     picture.width = '124';
@@ -51,10 +50,8 @@ var getReviewElement = function(data, container) {
 
   return element;
 };
-window.reviews.forEach(function(review) {
-  getReviewElement(review, reviewsContainer);
-});
-
+window.getReviewElement = getReviewElement;
+window.reviewsContainer = reviewsContainer;
 reviewsFiltersShow();
 
 function reviewsFiltersHide() {
