81a48945222fd2c0804de90b35f2177c37228c6e
Evgeny Ladyzhenskiy <7384093@gmail.com>
2016-06-15 09:42:22 +0300

    loadImage - now clobal function

--- src/reviews.js
+++ src/reviews.js
@@ -4,6 +4,7 @@ var reviewsFilter = document.querySelector('.reviews-filter');
 var reviewsContainer = document.querySelector('.reviews-list');
 var templateElement = document.querySelector('template');
 var elementToClone;
+var imageToLoadIn;
 
 reviewsFiltersHide();
 
@@ -22,23 +23,10 @@ if ('content' in templateElement) {
 var getReviewElement = function(data, container) {
   var element = elementToClone.cloneNode(true);
   var picture = element.querySelector('.review-author');
-  var avatarImage = new Image();
   element.querySelector('.review-text').textContent = data.description;
   container.appendChild(element);
 
-  function loadImage(url, onSuccess, onFailure) {
-    // грузим картинку
-    avatarImage.src = url;
-    // если всё хорошо
-    avatarImage.onload = function() {
-      onSuccess();
-    };
-
-    // если всё плохо
-    avatarImage.onerror = function() {
-      onFailure();
-    };
-  }
+  imageToLoadIn = new Image();
 
   loadImage(data.author.picture, function() {
     picture.src = data.author.picture;
@@ -47,9 +35,24 @@ var getReviewElement = function(data, container) {
   }, function() {
     element.classList.add('review-load-failure');
   });
-
   return element;
 };
+console.log(loadImage);
+console.log(imageToLoadIn);
+var loadImage = function(url, onSuccess, onFailure) {
+  // грузим картинку
+  imageToLoadIn.src = url;
+  // если всё хорошо
+  imageToLoadIn.onload = function() {
+    onSuccess();
+  };
+
+  // если всё плохо
+  imageToLoadIn.onerror = function() {
+    onFailure();
+  };
+};
+
 __loadCallback('//up.htmlacademy.ru/assets/js_intensive/jsonp/reviews.js', function(data) {
   window.reviews = data;
   window.reviews.forEach(function(review) {
