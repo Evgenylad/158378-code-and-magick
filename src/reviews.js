1b42615f85ff9dc56da4790f57d92ebb1784bbf9
Evgeny Ladyzhenskiy <7384093@gmail.com>
2016-06-25 12:03:34 +0300

    eventListener corrected

--- src/reviews.js
+++ src/reviews.js
@@ -254,7 +254,8 @@ var setFilter = function(filter) {
 var setFilterEnabled = function() {
   var filters = document.getElementsByName('reviews');
   reviewsFilter.addEventListener('click', function(evt) {
-    if(evt.target.tagName.toUpperCase() === 'INPUT') {
+    if(evt.target.id !== '') {
+      console.log(evt.target.id);
       setFilter(evt.target.id);
     }
   });
