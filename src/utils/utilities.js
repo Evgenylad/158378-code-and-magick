ec2a723fccd77a10823157b12876439d441361b0
Evgeny Ladyzhenskiy <7384093@gmail.com>
2016-06-30 18:32:27 +0300

    AMD modules define-level starts

--- src/utils/utilities.js
+++ src/utils/utilities.js
@@ -1,13 +1,33 @@
 'use strict';
 define(function() {
-  return {
-    checkField: function(input) {
-      var inputLength = input.value.length;
-      if (inputLength < 1 && input.required) {
-        return false;
-      } else {
-        return true;
-      }
+  var form = document.querySelector('.review-form');
+  var submitButton = form.querySelector('.review-submit');
+  var reviewFieldsContainer = form.querySelector('.review-fields');
+
+  function _checkField(input) {
+    var inputLength = input.value.length;
+    if (inputLength < 1 && input.required) {
+      return false;
+    } else {
+      return true;
     }
+  }
+
+  // Enabling form
+  function _enableForm() {
+    reviewFieldsContainer.style.visibility = 'hidden';
+    submitButton.removeAttribute('disabled');
+  }
+
+  // Disabling form
+  function _disableForm() {
+    reviewFieldsContainer.style.visibility = 'visible';
+    submitButton.setAttribute('disabled', 'disabled');
+  }
+
+  return {
+    checkField: _checkField,
+    enableForm: _enableForm,
+    disableFrom: _disableForm
   };
 });
