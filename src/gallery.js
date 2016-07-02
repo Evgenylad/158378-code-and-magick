68ca9252ff5c76ac1bf4b4d7d32ff30e744f68c5
Evgeny Ladyzhenskiy <7384093@gmail.com>
2016-07-02 12:31:00 +0300

    gallery popup v.1.1

--- src/gallery.js
+++ src/gallery.js
@@ -1,57 +1,82 @@
 'use strict';
 define(function() {
   function galleryModule() {
+    /** @constant {number} */
+    var PICTURES_AT_PHOTOGALLERY = 6;
+
     var galleryContainer = document.querySelector('.overlay-gallery');
     var previewContainer = galleryContainer.querySelector('.overlay-gallery-preview');
     var photogallery = document.querySelector('.photogallery');
-    var galleryItem;
-    var thumbnails;
     var collectionOfImages = photogallery.getElementsByTagName('img');
     var arrayOfImages = Array.prototype.slice.call(collectionOfImages);
     var arrayOfImageSrc = arrayOfImages.map(function(img) {
       return img.src;
     });
 
+    var controlLeft = galleryContainer.querySelector('.overlay-gallery-control-left');
+    var controlRight = galleryContainer.querySelector('.overlay-gallery-control-right');
+
     /**@type {number}*/
     var galleryActivePicture = 0;
-
-    /**@type {number} pic*/
-    var setGalleryActivePictures = function(pic) {
-      thumbnails = previewContainer.querySelectorAll('.gallery-thumbnails-image');
-      thumbnails[pic].classList.add('active');
-      previewContainer.querySelector('.active').style.border = '4px solid #ffffff';
-      galleryItem.src = thumbnails[pic].src;
-      console.log(previewContainer.querySelector('.active'));
-    };
 
     /**@param {Array.<string>}*/
     var galleryPictures = [];
 
     /**@param {Array.<string>} pictures*/
-    var showGallery = function(pictures) {
+    var loadImages = function(pictures) {
       galleryPictures = pictures;
-      galleryPictures.forEach(function(pic) {
-        var preview = new Image();
-        preview.classList.add('gallery-thumbnails-image');
-        previewContainer.appendChild(preview);
-        preview.src = pic;
-        preview.width = '100';
-        preview.height = '100';
-      });
-      galleryActivePicture = 0;
-      setGalleryActivePictures(galleryActivePicture);
+      return galleryPictures;
+    };
+
+    /**@type {number} pic*/
+    var showGallery = function(pic) {
+      galleryActivePicture = pic;
       galleryContainer.classList.remove('invisible');
+      showPicture(loadImages(arrayOfImageSrc), galleryActivePicture);
+      if(_galleryMoveLeft()) {
+        showPicture(loadImages(arrayOfImageSrc), galleryActivePicture);
+      }
+      if(_galleryMoveRight()) {
+        showPicture(loadImages(arrayOfImageSrc), galleryActivePicture);
+      }
     };
 
     var hideGallery = function() {
       galleryContainer.classList.add('invisible');
     };
 
+    var _galleryMoveLeft = function() {
+      controlLeft.addEventListener('click', function() {
+        if(galleryActivePicture >= 0) {
+          console.log(galleryActivePicture);
+          galleryActivePicture--;
+        }
+        return galleryActivePicture;
+      });
+    };
+
+    var _galleryMoveRight = function() {
+      controlRight.addEventListener('click', function() {
+        if(galleryActivePicture <= PICTURES_AT_PHOTOGALLERY) {
+          console.log(galleryActivePicture);
+          galleryActivePicture++;
+        }
+        return galleryActivePicture;
+      });
+    };
+
+    var showPicture = function(pictures, pic) {
+      console.log(pictures);
+      galleryPictures = pictures;
+      var preview = new Image();
+      preview.classList.add('gallery-fullscreen-image');
+      previewContainer.appendChild(preview);
+      preview.src = galleryPictures[pic];
+    };
+
     arrayOfImages.forEach(function(item) {
-      item.classList.add('gallery-item');
-      galleryItem = item;
       item.addEventListener('click', function() {
-        showGallery(arrayOfImageSrc);
+        showGallery(galleryActivePicture);
       });
     });
 
