524e65e7a6963c0944277796a6294f50117cee6a
Evgeny Ladyzhenskiy <7384093@gmail.com>
2016-07-03 12:56:38 +0300

    code refactored

--- src/gallery.js
+++ src/gallery.js
@@ -1,100 +1,99 @@
 'use strict';
-define(['./utils/arrayOfImages', './utils/showAnyGallery'], function(arrayOfImagesModule, showAnyGalleryModule) {
-  function galleryModule() {
+define(function() {
 
-    /** @constant {number} */
-    var KEYCODE_ESCAPE = 27;
+  /** @constant {number} */
+  var KEYCODE_ESCAPE = 27;
 
-    var arrayOfLinks = document.querySelectorAll('.photogallery-image');
+  var galleryContainer = document.querySelector('.overlay-gallery');
+  var previewContainer = galleryContainer.querySelector('.overlay-gallery-preview');
 
-    var galleryContainer = document.querySelector('.overlay-gallery');
-    var previewContainer = galleryContainer.querySelector('.overlay-gallery-preview');
+  var controlLeft = galleryContainer.querySelector('.overlay-gallery-control-left');
+  var controlRight = galleryContainer.querySelector('.overlay-gallery-control-right');
+  var controlCross = galleryContainer.querySelector('.overlay-gallery-close');
 
-    var controlLeft = galleryContainer.querySelector('.overlay-gallery-control-left');
-    var controlRight = galleryContainer.querySelector('.overlay-gallery-control-right');
-    var controlCross = galleryContainer.querySelector('.overlay-gallery-close');
+  /** @type {number} */
+  var picturesAtPhotogallery = 6;
 
-    /** @type {number} */
-    var picturesAtPhotogallery = 6;
+  /**@type {number}*/
+  var galleryActivePicture;
 
-    /**@type {number}*/
-    var galleryActivePicture = 0;
+  /**@type {Array.<string>}*/
+  var galleryPictures = [];
 
-    /**@type {Array.<string>}*/
-    var galleryPictures = [];
+  /**@param {Array.<string>} pictures
+   * @return {Array.<string>}
+   * */
 
-    /**@param {Array.<string>} pictures
-     * @return {Array.<string>}
-     * */
-    var saveImages = function(pictures) {
-      galleryPictures = pictures;
-      return galleryPictures;
-    };
+  function saveImages(pictures) {
+    galleryPictures = pictures;
+    return galleryPictures;
+  }
 
-    var _attachListeners = function() {
-      _galleryMoveLeft();
-      _galleryMoveRight();
-      _galleryHide();
-    };
+  var _attachListeners = function() {
+    _galleryMoveLeft();
+    _galleryMoveRight();
+    _galleryHide();
+  };
 
-    var removeEventListeners = function() {
-      controlLeft.removeEventListener('click', showLeftPicture);
-      controlRight.removeEventListener('click', showRightPicture);
-    };
+  var removeEventListeners = function() {
+    controlLeft.removeEventListener('click', showLeftPicture);
+    controlRight.removeEventListener('click', showRightPicture);
+  };
 
-    var _galleryMoveLeft = function() {
-      controlLeft.addEventListener('click', showLeftPicture);
-    };
+  var _galleryMoveLeft = function() {
+    controlLeft.addEventListener('click', showLeftPicture);
+  };
 
-    var _galleryMoveRight = function() {
-      controlRight.addEventListener('click', showRightPicture);
-    };
+  var _galleryMoveRight = function() {
+    controlRight.addEventListener('click', showRightPicture);
+  };
 
-    var _galleryHide = function() {
-      removeEventListeners();
-      document.addEventListener('keydown', function(evt) {
-        if (evt.keyCode === KEYCODE_ESCAPE) {
-          galleryContainer.classList.add('invisible');
-        }
-      });
-
-      controlCross.addEventListener('click', function() {
+  var _galleryHide = function() {
+    document.addEventListener('keydown', function(evt) {
+      if (evt.keyCode === KEYCODE_ESCAPE) {
         galleryContainer.classList.add('invisible');
-      });
-    };
-
-    var showLeftPicture = function() {
-      if(galleryActivePicture > 0) {
-        galleryActivePicture--;
-        showPicture(saveImages(arrayOfImagesModule().arrayOfImageSrc), galleryActivePicture);
+        removeEventListeners();
       }
-      return galleryActivePicture;
-    };
-
-    var showRightPicture = function() {
-      if(galleryActivePicture < picturesAtPhotogallery) {
-        galleryActivePicture++;
-        showPicture(saveImages(arrayOfImagesModule().arrayOfImageSrc), galleryActivePicture);
-      }
-      return galleryActivePicture;
-    };
-
-    var showPicture = function(pictures, pic) {
-      previewContainer.innerHTML = '';
-      galleryPictures = pictures;
-      var preview = new Image();
-      preview.classList.add('gallery-fullscreen-image');
-      previewContainer.appendChild(preview);
-      preview.src = galleryPictures[pic];
-    };
-
-    showAnyGalleryModule(arrayOfLinks, function showGallery(pic) {
-      galleryActivePicture = pic;
-      galleryContainer.classList.remove('invisible');
-      showPicture(saveImages(arrayOfImagesModule().arrayOfImageSrc), galleryActivePicture);
-      _attachListeners();
     });
 
+    controlCross.addEventListener('click', function() {
+      galleryContainer.classList.add('invisible');
+      removeEventListeners();
+    });
+  };
+
+  var showLeftPicture = function() {
+    if(galleryActivePicture > 0) {
+      galleryActivePicture--;
+      showPicture(galleryActivePicture);
+    }
+    return galleryActivePicture;
+  };
+
+  var showRightPicture = function() {
+    if(galleryActivePicture < picturesAtPhotogallery) {
+      galleryActivePicture++;
+      showPicture(galleryActivePicture);
+    }
+    return galleryActivePicture;
+  };
+
+  var showPicture = function(pic) {
+    var preview = new Image();
+    preview.classList.add('gallery-fullscreen-image');
+    previewContainer.appendChild(preview);
+    preview.src = galleryPictures[pic];
+  };
+
+  function showGallery(pic) {
+    galleryActivePicture = pic;
+    galleryContainer.classList.remove('invisible');
+    showPicture(galleryActivePicture);
+    _attachListeners();
   }
-  return galleryModule;
+
+  return {
+    saveImages: saveImages,
+    showGallery: showGallery
+  };
 });
