ddc234aac739c09d46762afd896850b00b237cf1
Evgeny Ladyzhenskiy <7384093@gmail.com>
2016-07-04 16:32:59 +0300

    constructor v.1

--- src/utils/getReviewElement.js
+++ src/utils/getReviewElement.js
@@ -1,30 +1,33 @@
 'use strict';
 define(['./loadImage'], function(loadImage) {
 
-  function getReviewElement(data, container, elementToClone) {
-    var element = elementToClone.cloneNode(true);
-    var picture = element.querySelector('.review-author');
-    element.querySelector('.review-text').textContent = data.description;
-    var rating = element.querySelector('.review-rating');
+  var Review = function(data, container, elementToClone) {
+    this.data = data.author.picture;
+    this.element = function() {
+      var element = elementToClone.cloneNode(true);
+      var picture = element.querySelector('.review-author');
+      element.querySelector('.review-text').textContent = data.description;
+      var rating = element.querySelector('.review-rating');
 
-    var ratingValue = data.rating;
-    var ratingNames = ['', '', 'two', 'three', 'four', 'five'];
-    if(ratingValue >= 2 && ratingValue <= 5) {
-      rating.classList.add('review-rating-' + ratingNames[ratingValue]);
-    }
+      var ratingValue = data.rating;
+      var ratingNames = ['', '', 'two', 'three', 'four', 'five'];
+      if(ratingValue >= 2 && ratingValue <= 5) {
+        rating.classList.add('review-rating-' + ratingNames[ratingValue]);
+      }
 
-    container.appendChild(element);
+      container.appendChild(element);
+      loadImage(data.author.picture, function() {
+        picture.src = data.author.picture;
+        picture.width = '124';
+        picture.height = '124';
+      }, function() {
+        element.classList.add('review-load-failure');
+      });
+      return element;
+    };
+    this.remove = function() {
 
-    loadImage(data.author.picture, function() {
-      picture.src = data.author.picture;
-      picture.width = '124';
-      picture.height = '124';
-    }, function() {
-      element.classList.add('review-load-failure');
-    });
-    return element;
-  }
-
-  return getReviewElement;
-
+    };
+  };
+  return Review;
 });
